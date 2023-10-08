493ac756b178d638bb1ea766f2741448e9d6ff69
Ivan Volodin <ivanwolodin@gmail.com>
2023-10-08 16:32:12 +0500

    Add: mixin class for pagination

--- docker_compose/app/movies/api/v1/views.py
+++ docker_compose/app/movies/api/v1/views.py
@@ -6,9 +6,10 @@ from django.views.generic.list import BaseListView
 from movies.models import FilmWork, PersonFilmwork
 
 ROLES = PersonFilmwork.Roles
+PAGINATE_BY = 50
 
 
-class MoviesListApi(BaseListView):
+class MoviesApiMixin(BaseListView):
     model = FilmWork
     http_method_names = ['get']
 
@@ -33,11 +34,34 @@ class MoviesListApi(BaseListView):
         )
         return query_set
 
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = {
-            'results': list(self.get_queryset()),
-        }
-        return context
+    # def get_context_data(self, *, object_list=None, **kwargs):
+    #     context = {
+    #         'results': list(self.get_queryset()),
+    #     }
+    #     return context
 
     def render_to_response(self, context, **response_kwargs):
         return JsonResponse(context)
+
+
+class MoviesListApi(MoviesApiMixin, BaseListView):
+    paginate_by = PAGINATE_BY
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        queryset = self.get_queryset()
+        paginator, page, queryset, is_paginated = self.paginate_queryset(
+            queryset,
+            MoviesListApi.paginate_by
+        )
+
+        context = {
+            'count': paginator.count,
+            "total_pages": paginator.num_pages,
+            "prev":
+                page.previous_page_number() if page.has_previous() else None,
+            "next":
+                page.next_page_number() if page.has_next() else None,
+            "results": list(queryset),
+        }
+
+        return context
